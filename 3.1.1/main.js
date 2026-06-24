ac86dce565b61e37d949fa1dec8b9a87768d5bac
Trex1023 <121815633+Trex1023@users.noreply.github.com>
2026-06-24 16:23:12 -0300

    Update main.js

--- 3.1.1/main.js
+++ 3.1.1/main.js
@@ -6,3 +6,6 @@ const catalogo = [
 { id: 5, titulo: 'The Miseducation', artista: 'Lauryn Hill', genero: 'RnB', anio: 1998, precio: 1100, disponible: true, canciones: ['Ex-Factor', 'Doo Wop', 'Everything is Everything'] }
 ];
 
+let gen = toLowerCase(prompt("¿Que genero le interesa?"))
+const repuesta = catalogo.filter(catalogo => toLowerCase(catalogo.genero) == gen)
+console.log(repuesta)
